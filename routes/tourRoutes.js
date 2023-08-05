c67fd355f05d967b034ccdf07ac843406e36e24c
mido4315 <mohmed.mahmoud3@gmail.com>
2023-08-05 15:40:31 +0300

    use middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const {
   getAllTours,
   createTour,
@@ -9,6 +10,8 @@ const {
 
 const router = express.Router();
 
+// router.param('id', checkID);
+ 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTourById).patch(updateTour).delete(deleteTour);
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,10 @@ router
   .route('/')
   .get(userController.getAllUsers)
   .post(userController.createUser);
-//router.route('/:id').get(getUserById).patch(updateUser).delete(deleteUser);
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
