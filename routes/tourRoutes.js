04156169882729d7fe467a0e0a224d984c103af9
mido4315 <mohmed.mahmoud3@gmail.com>
2023-08-11 02:04:38 +0300

    update tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,13 +6,19 @@ const {
   getTourById,
   updateTour,
   deleteTour,
+  topCheapest,
+  getTourStatus,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
 // router.param('id', checkID);
- 
 router.route('/').get(getAllTours).post(createTour);
+
+router.route('/stats').get(getTourStatus);
+
+router.route('/top-cheapest').get(topCheapest, getAllTours);
+
 router.route('/:id').get(getTourById).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
