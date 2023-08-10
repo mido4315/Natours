200134c533c2c8d7c775d4b81a39c2d9a72be70e
mido4315 <mohmed.mahmoud3@gmail.com>
2023-08-11 02:03:51 +0300

    create and use import script

---
+++ dev-data/data/import-script.js
@@ -0,0 +1,52 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({
+  path: './config.env',
+});
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection established successfully'))
+  .catch(() => console.log('DB connection failed'));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data saved successfully');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany({});
+    console.log('Data deleted successfully');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
