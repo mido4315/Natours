2df348653e6b8cf51f053e603bf582deb341accf
mido4315 <mohmed.mahmoud3@gmail.com>
2023-08-26 14:26:36 +0300

    creating user model

---
+++ models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please enter a valid email'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (val) {
+        return val === this.password;
+      },
+      message: 'Passwords are not typical',
+    },
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
